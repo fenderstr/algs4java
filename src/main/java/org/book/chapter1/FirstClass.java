ca8b3af2b09453134535ed3f40ef98021623e954
anosov <alexandern89@gmail.com>
2013-03-21 23:58:09 +0400

    initial

---
+++ src/main/java/org/book/chapter1/FirstClass.java
@@ -0,0 +1,10 @@
+package org.book.chapter1;
+
+/**
+ * @author root
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        stdlib.org.algs4java.stdlib.BinaryStdIn.
+    }
+}
